4e24a3dcf7c375d0c735e15fec59295ff4c7a443
Roshan <roshanbhgt@gmail.com>
2014-01-25 22:13:33 +0530

    Updates

--- login.php
+++ login.php
@@ -9,11 +9,22 @@
 		if(!validateEmail($data['email'])){
 			echo "Please enter a valid email address.";
 		}else{
-			if($objUser->loginUser($data)){				
-				header('Location: http://'.$_SERVER['HTTP_HOST'].'/buisinessdiary/myaccount.php/');
-				exit;
+			if($objUser->loginUser($data)){	
+				/*
+					if (headers_sent() === false)
+					{
+						header('Location: ' . $url, true, ($permanent === true) ? 301 : 302);
+					}
+					exit();
+				*/
+				$url = 'http://'.$_SERVER['HTTP_HOST'].'/buisinessdiary/myaccount.php/';
+				$response['success'] = 1;
+				$response['url'] = $url;
+				echo json_encode($response);
 			}else{
-				echo "Invalid login details entered. Please try again later in sometime...";
+				$response['success'] = 0;
+				$response['message'] = "Invalid login details entered. Please try again later in sometime...";
+				echo json_encode($response);
 			}
 		}
 	}
